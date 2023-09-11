ead9a3cbd0c9972de674d6f6747fd02e1c00e186
IvanFan <3323730286@qq.com>
2023-09-11 15:11:26 +0800

    import Roboto font

--- src/question5/src/main.jsx
+++ src/question5/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/style/fonts.css";
+import "./assets/style/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
